b382f5b797ac257146410c76ee8988eeb9f71708
raydiaz3000 <raydiaz3000@gmail.com>
2022-08-22 11:14:20 -0700

    Add project files.

---
+++ TestMsinContainer3/Controllers/TestAppContainerController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAppContainer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestAppContainerController : ControllerBase
+    {
+        private static readonly string[] Activities = new[]
+        {
+            "Login Test"
+        };
+
+        private readonly ILogger<TestAppContainerController> _logger;
+
+        public TestAppContainerController(ILogger<TestAppContainerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<LoginTemplate> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new LoginTemplate
+            {
+                Activity = Activities[rng.Next(Activities.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ TestMsinContainer3/LoginTemplate.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TestAppContainer
+{
+    public class LoginTemplate
+    {
+
+        public int LoginID = 15;
+
+        public string Activity { get; set; }
+    }
+}
